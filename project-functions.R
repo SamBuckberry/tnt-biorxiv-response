8d516bbb270778fad35ace20c9efd25c158a0279
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2024-06-17 19:41:45 +0800

    change font size for ggplot

--- project-functions.R
+++ project-functions.R
@@ -39,10 +39,10 @@ sams_pub_theme <- function(legend_pos = "none", x.text.angle = 45, hjust = 1,
               panel.grid.minor = element_blank(),
               panel.grid.major = element_line(size = line_mm),
               panel.border = element_rect(color = "black", size = line_mm, fill = NA), # Add border around each facet
-              axis.text.x = element_text(angle = x.text.angle, hjust = hjust, size = 6, colour = 'black'),
-              axis.text.y = element_text(size = 6, colour = 'black', angle = y.text.angle),
-              strip.text.y = element_text(size = 6),
-              text = element_text(size = 6),
+              axis.text.x = element_text(angle = x.text.angle, hjust = hjust, size = 8, colour = 'black'),
+              axis.text.y = element_text(size = 8, colour = 'black', angle = y.text.angle),
+              strip.text.y = element_text(size = 8),
+              text = element_text(size = 8),
               strip.background = element_blank(),
               legend.position = legend_pos,
               axis.line.x = element_line(color = 'black', size = line_mm),
@@ -50,25 +50,4 @@ sams_pub_theme <- function(legend_pos = "none", x.text.angle = 45, hjust = 1,
               axis.ticks = element_line(color = 'black', size = line_mm))
     return(custom_theme)
 }
-
-#
-# sams_pub_theme <- function(legend_pos = "NA", x.text.angle = 45, hjust=1,
-#                            y.text.angle=0, line_point=0.5){
-#     line_mm <- line_point / 2.835
-#     custom_theme <- theme_bw() +
-#         theme(plot.background = element_blank(),
-#               panel.grid.minor = element_blank(),
-#               panel.grid.major = element_line(size = line_mm),
-#               panel.border = element_blank(),
-#               axis.text.x = element_text(angle = x.text.angle, hjust = hjust, size = 6,
-#                                          colour = 'black'),
-#               axis.text.y = element_text(size=6, colour='black', angle = y.text.angle),
-#               strip.text.y = element_text(size = 6),
-#               text = element_text(size=6),
-#               strip.background = element_blank(),
-#               legend.position = legend_pos,
-#               axis.line.x = element_line(color = 'black', size = line_mm),
-#               axis.line.y = element_line(color = 'black', size = line_mm),
-#               axis.ticks = element_line(color = 'black', size = line_mm))
-#     return(custom_theme)
-# }
+s
